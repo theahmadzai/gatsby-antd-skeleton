8443eb3c11b2e3f5f6759d3d286f0854e505e080
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-10-24 01:21:34 +0500

    add dummy page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,6 @@
-//
+exports.createPages = ({ actions: { createPage } }) => {
+  createPage({
+    path: '/dummy',
+    component: require.resolve('./src/templates/Dummy/Dummy.js'),
+  })
+}
---
+++ src/templates/Dummy/Dummy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+import styles from './Dummy.module.less'
+
+const DummyTemplate = () => (
+  <Layout>
+    <h1 className={styles.dummy}>Dummy Template</h1>
+  </Layout>
+)
+
+export default DummyTemplate
